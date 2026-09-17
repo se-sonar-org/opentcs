2b0b76aae5ff42d026463811d9736ec7f0dbc8a9
vortex-ab <vortex-ab@local>
2026-09-17 02:07:14 +0200

    vortex-ab without: player-lookup (analysis-mu4qff53-ba9e2e86-1789601334378)

--- opentcs-kernel-extension-http-services/src/test/java/org/opentcs/kernel/extensions/servicewebapi/v1/VehicleHandlerTest.java
+++ opentcs-kernel-extension-http-services/src/test/java/org/opentcs/kernel/extensions/servicewebapi/v1/VehicleHandlerTest.java
@@ -4,6 +4,7 @@ package org.opentcs.kernel.extensions.servicewebapi.v1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.entry;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -36,6 +37,7 @@ import org.opentcs.data.model.Path;
 import org.opentcs.data.model.Point;
 import org.opentcs.data.model.Vehicle;
 import org.opentcs.drivers.vehicle.VehicleCommAdapterDescription;
+import org.opentcs.drivers.vehicle.VehicleCommAdapterMessage;
 import org.opentcs.drivers.vehicle.management.VehicleAttachmentInformation;
 import org.opentcs.kernel.extensions.servicewebapi.KernelExecutorWrapper;
 import org.opentcs.kernel.extensions.servicewebapi.v1.binding.GetVehicleResponseTO;
@@ -262,6 +264,31 @@ class VehicleHandlerTest {
   void throwOnSetEnvelopeUnknownVehicle() {
     assertThatExceptionOfType(ObjectUnknownException.class)
         .isThrownBy(() -> handler.putVehicleEnvelopeKey("some-unknown-vehicle", "some-key"));
+  }
+
+  @Test
+  void setVehicleCommAdapterPosition() {
+    // Act
+    handler.postVehicleCommAdapterPosition("some-vehicle", "some-point");
+
+    // Assert
+    ArgumentCaptor<VehicleCommAdapterMessage> captor
+        = ArgumentCaptor.forClass(VehicleCommAdapterMessage.class);
+    then(vehicleService)
+        .should()
+        .sendCommAdapterMessage(eq(vehicle.getReference()), captor.capture());
+    assertThat(captor.getValue().getType()).isEqualTo("tcs:virtualVehicle:setPosition");
+    assertThat(captor.getValue().getParameters()).containsExactly(
+        entry("position", "some-point")
+    );
+  }
+
+  @Test
+  void throwOnSetCommAdapterPositionForUnknownVehicle() {
+    assertThatExceptionOfType(ObjectUnknownException.class)
+        .isThrownBy(
+            () -> handler.postVehicleCommAdapterPosition("some-unknown-vehicle", "some-point")
+        );
   }
 
   @Test
